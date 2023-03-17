16fd2d905d7c8b9bab6003f25c7c4d78f4cd1101
liuyanxing <liuyanxing@jwzg.com>
2023-03-17 11:55:20 +0800

    add lsp lint format server; fix clangd error

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,20 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        "lua_ls",
+        "clangd",
+        "cmake",
+        "cssls",
+        "eslint",
+        "html",
+        "jsonls",
+        "tsserver",
+        "marksman",
+        "pyright",
+        "volar",
+        "wgsl_analyzer",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,14 +26,14 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = { "prettier", "stylua", "clang-format", "eslint_d" },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      -- ensure_installed = { "python" },
+      ensure_installed = { "codelldb", "node2", "python" },
     },
   },
 }
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua", "cpp", "cmake", "bash", "css", "dockerfile", "gitignore", "glsl", "javascript", "json", "python", "tsx", "typescript", "wgsl" },
   },
 }
